b7795843e89634186f943a9b63c77641308be3f3
ykjang <yongkyu.jang@gmail.com>
2014-07-03 19:18:13 +0900

    batch Job 수

---
+++ SC_MITS/src/main/java/com/isec/sc/intgr/scheduler/OrderCreateJobBean.java
@@ -0,0 +1,48 @@
+/*
+ *  
+ *  * Revision History
+ *  * Author              Date                  Description
+ *  * ------------------   --------------       ------------------
+ *  *  beyondj2ee          2014.01.02              
+ *  
+ */
+
+package com.isec.sc.intgr.scheduler;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+
+public class OrderCreateJobBean extends QuartzJobBean {
+
+    private OrderCreateTask orderCreateTask;
+    private String redisKey;
+    private String redisPushKey;
+    private String redisErrKey;
+
+    protected void executeInternal(JobExecutionContext context)
+            throws JobExecutionException {
+
+    	
+    	orderCreateTask.createOrder(redisKey, redisPushKey, redisErrKey);
+
+    }
+
+
+    public void setOrderCreateTask(OrderCreateTask orderCreateTask) {
+        this.orderCreateTask = orderCreateTask;
+    }
+    
+    public void setRedisKey(String redisKey){
+    	this.redisKey = redisKey;
+    }
+    
+    public void setRedisPushKey(String redisPushKey){
+    	this.redisPushKey = redisPushKey;
+    }
+    
+    public void setRedisErrKey(String redisErrKey){
+    	this.redisErrKey = redisErrKey;
+    }
+}
